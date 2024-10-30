ccdfb4f7a694be6d682bb1e73770c191fdee3883
Anésio Sousa <39014361+AnesioSousa@users.noreply.github.com>
2024-10-30 11:15:33 -0300

    Add files via upload

--- gpu/gpu_lib.c
+++ gpu/gpu_lib.c
@@ -2,21 +2,30 @@
 #include <stdint.h>
 
 extern void mem_map();
-extern void set_background_block(uint32_t line);
+extern void set_background_block(uint32_t line, uint32_t col, uint32_t B, uint32_t G, uint32_t R);
 extern void clear_background();
 extern void mem_unmap();
+void apagar_Tudo();
 
 void iniciar_tela(){
   int i, j;
-mem_map();
-  clear_background();
-
-  for(i=40; i<50; i++)
-      set_background_block(i);
+  mem_map();
+  //clear_background();
+  //for(i=0; i<3; i++)
+  //set_background_block(1, 0, 0, 0, 7);
+  apagar_Tudo();
 }
 
 void desmapear_tela(){
   mem_unmap();
 }
 
+void apagar_Tudo(){
+    int i, j;
+  
+    for (j=0; j <= 79; j++){
+            set_background_block(0, j, 7, 0, 0);
+    }
+
+}
 
--- gpu/lib.s
+++ gpu/lib.s
@@ -11,12 +11,6 @@
 .global set_background_block
 .global clear_background
 
-.macro print value display
-	 add r6, r10, #\display
-	 mov r0, #\value
-	 str r0, [r6]
-.endm
-
 .type mem_map, %function
 mem_map:
     sub sp, sp, #28
@@ -131,53 +125,55 @@ clear_background:
 
 .type set_background_block, %function
 set_background_block:
-    sub sp, sp, #28
-    str r6, [sp, #24]  
-    str r5, [sp, #20]
-    str r4, [sp, #16]
-    str r3, [sp, #12]
-    str r2, [sp, #8]
-    str r1, [sp, #4]
-    str r0, [sp, #0]
-
-    ldr r6, =mem_mapped_file_descriptor
-    ldr r6, [r6]
+    ldr r4, [sp, #0]
     
-    add r3, r6, #0x80 @ Carrega DATA_A_BASE em r3
+    sub sp, sp, #32
+    str r11, [sp, #28]
+    str r10, [sp, #24]
+    str r9, [sp, #20]
+    str r8, [sp, #16]
+    str r7, [sp, #12]
+    str r6, [sp, #8]  
+    str r5, [sp, #4]
+
+    ldr r10, =mem_mapped_file_descriptor
+    ldr r10, [r10]
+    
+    add r7, r10,  #0xc0 @ Carrega start_signal em r5
+    add r6, r10, #0x70 @ Carrega DATA_B_BASE em r4
+    add r5, r10, #0x80 @ Carrega DATA_A_BASE em r3
     
     @Calculo do endereco
-    mov r1, r0 @r0 vem da chamada em C (arquivo gpu_lib.c)
-    mov r2, #30
-    mul r1, r2, r1
-    lsl r1, #4
-    orr r1, #0b0010  @opcode
-    str r1, [r3]
-
-    add r4, r6, #0x70 @ Carrega DATA_B_BASE em r4
+    mov r8, r0 
+    mov r9, r1
+    mov r11, #80
+    mul r8, r11, r8
+    add r8, r8, r9
+    lsl r8, #4
+    orr r8, #0b0010  @opcode
+    str r8, [r5] @escreve em data_A
     
-    mov r1, #0b000
-    lsl r1, #3
-    orr r1, #0b000
-    lsl r1, #3
-    orr r1, #0b000
-    str r1, [r4]
+    mov r8, r2
+    lsl r8, #3
+    orr r8, r3
+    lsl r8, #3
+    orr r8, r4
+    str r8, [r6] @escreve em data_B
 
-    add r5, r6,  #0xc0 @ Carrega start_signal em r5
-    
-    mov r1, #0 
-    str r1, [r5]
+    mov r8, #0 
+    str r8, [r7] @escreve em start_signal
  
-    mov r1, #1 
-    str r1, [r5]
+    mov r8, #1 
+    str r8, [r7] @escreve em start_signal
     
-    ldr r6, [sp, #24]
-    ldr r5, [sp, #20]
-    ldr r4, [sp, #16]
-    ldr r3, [sp, #12]
-    ldr r2, [sp, #8]
-    ldr r1, [sp, #4]
-    ldr r0, [sp, #0]
-    add sp, sp, #28
+    ldr r11, [sp, #28]
+    ldr r10, [sp, #24]
+    ldr r9, [sp, #20]   
+    ldr r8, [sp, #16]
+    ldr r7, [sp, #12]    
+    ldr r6, [sp, #8]
+    ldr r5, [sp, #4]
+    add sp, sp, #32
 
     bx lr
 
